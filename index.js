46a63687fcb987ef87580fab82ff4f71f4a64eee
Naresh Bohara <nareshbohara3305@gmail.com>
2025-04-14 23:15:28 +0545

    code updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.PORT);
+
 
 import http from "http" 
 import application from "./src/config/express.config.js";
  
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || "0.0.0.0";
-  
+const port = process.env.PORT || 9005;
+const host = process.env.HOST || "127.1.0.0";
+   
 const appServer = http.createServer(application);
 
 appServer.listen(port, host, (error)=>{
